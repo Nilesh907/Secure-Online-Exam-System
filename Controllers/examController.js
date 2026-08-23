7215a7a773ea30b8c0b60081c68ac5984fa6479f
Nilesh Sen <nileshsen873@gmail.com>
2026-08-23 18:36:15 +0530

    Fix exam timing and token validation

--- Controllers/examController.js
+++ Controllers/examController.js
@@ -49,6 +49,12 @@ exports.generateToken = async (req, res) => {
             return res.send("Token allowed only 2 minutes before exam");
         }
 
+
+        if (now > paper.endTime) {
+            return res.send(
+                "Exam has already ended"
+            );
+
         let session = await ExamSession.findOne({
             student: req.session.userId,
             paper: paper._id,
